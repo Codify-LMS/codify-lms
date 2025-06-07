4712410af1545e6607339d2aa6366cec5cf35380
s-erzv <sarahfajriarahmah@gmail.com>
2025-06-07 22:28:46 +0700

    first commit

---
+++ backend/codify-lms/src/main/java/com/codify/codify_lms/CodifyLmsApplication.java
@@ -0,0 +1,13 @@
+package com.codify.codify_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodifyLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodifyLmsApplication.class, args);
+	}
+
+}
